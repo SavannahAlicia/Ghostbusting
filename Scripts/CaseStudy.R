c548e86ac6daa6b86908d6fef44f28d0dd1011ea
Abinand Reddy <abinandkr@gmail.com>
2023-09-20 10:52:15 +0100

    manuscript publishing edits

---
+++ Scripts/CaseStudy.R
@@ -0,0 +1,106 @@
+library(secr)
+library(tidyverse)
+library(raster)
+library(ggpubr)
+library(patchwork)
+library(rlang)
+
+source('Scripts/2Encounter_functions.R')
+
+
+
+###Read data
+mesh = read_rds('Data/mesh.rds')
+all_traps = read_rds('Data/traps.rds')
+caps = read.csv('Data/captures.csv')
+
+site_names <- c("Munkhkhairkhan","Tost")   
+
+#### Areas of each site
+data.frame(sites = site_names, area = sapply(mesh, function(x) spacing(x)^2*nrow(x)/1000^2))
+
+#### Make capture history of data k  = 1
+Mongolia1 <- make.capthist(captures = caps,traps = all_traps,bysession = T)
+
+summary(Mongolia1,terse = T)
+
+#### Make capture history of data k  = 2
+Mongolia2 <- caps  %>% 
+  group_by(ID) %>% 
+  filter(n()>1) %>% 
+  data.frame() %>% 
+  make.capthist(captures = .,traps = all_traps,bysession = T) 
+
+summary(Mongolia2,terse = T)
+
+###### secr models
+
+scr_mods = lapply(1:length(site_names), function(x) secr.fit(capthist = Mongolia1[[x]],mask = mesh[[x]], model = list(D~value), detectfn = 'HHN'))
+
+scr2_mods = lapply(1:length(site_names), function(x) scr2_lik(Mongolia1[[x]],mesh[[x]], model = list(D~value),hessian = T))
+
+
+###### Extract estimates
+scr2_abund_f = function(phi,mesh){
+  b0 = phi[1]
+  b1 = phi[2]
+  sum(exp(b0 + b1*covariates(mesh)$value)*attr(mesh,'area'),na.rm = T)  
+}
+
+scr2_abund_se_f = function(phi,mesh,vcov){
+  grad2 <- nlme::fdHess(pars = phi, function(x) scr2_abund_f(x,mesh))$grad
+  scr2_se <- sqrt(t(grad2)%*%vcov%*%grad2)
+}
+
+scr2_coefs_f = function(fit,mesh){
+  EN = scr2_abund_f(fit$estimate[1:2],mesh)
+  EN_se = scr2_abund_se_f(fit$estimate[1:2],mesh,solve(fit$hessian)[1:2,1:2])  
+  theta = exp(fit$estimate[3:4])
+  se = sqrt(diag(solve(fit$hessian)))[3:4]*theta
+  return(data.frame(coef = c('EN','lambda0','sigma'), estimate = c(EN,theta), SE.estimate = c(EN_se,se)))
+}
+
+scr_coefs_f = function(fit){
+  EN = region.N(fit)[1,1:2]
+  theta = predict(scr_mods[[1]])[2:3,2:3]
+  (rbind(EN,theta) %>% mutate(coef = c('EN','lambda0','sigma')) )[,c(3,1,2)]
+}
+
+
+
+scr_coefs = lapply(1:length(site_names), function(x) scr_coefs_f(scr_mods[[x]]) %>% mutate(site = site_names[x], mod = 'scr'))
+scr2_coefs = lapply(1:length(site_names), function(x) scr2_coefs_f(scr2_mods[[x]],mesh = mesh[[x]]) %>% mutate(site = site_names[x], mod = 'scr2'))
+
+est_dat = do.call(rbind,c(scr_coefs,scr2_coefs)) %>% 
+  mutate(CV = SE.estimate/estimate)
+rownames(est_dat) <- NULL
+
+est_dat
+
+
+cap_stats <- function(caphist){
+  data.frame(caphist) %>% 
+    group_by(ID) %>% 
+    mutate(dets = n())  %>%
+    ungroup() %>% 
+    summarise(detections = n(), individuals = length(unique(ID)), single_detections = sum(dets == 1))
+}
+
+###capture history  summaries
+captures_table <- data.frame(t(sapply(Mongolia1,cap_stats))) 
+
+
+
+ggplot(mesh[[1]], aes(x = x, y = y))+
+  geom_tile(aes(fill = covariates(mesh[[1]])$value))+
+  scale_fill_viridis_c(name = 'Occupancy probability', limits = c(0,1))+
+  geom_point(data = traps(Mongolia1)[[1]], shape = 3, col = 'firebrick')+
+  coord_equal()+
+  theme_void()
+
+ggplot(mesh[[2]], aes(x = x, y = y))+
+  geom_tile(aes(fill = covariates(mesh[[2]])$value))+
+  scale_fill_viridis_c(name = 'Occupancy probability', limits = c(0,1))+
+  geom_point(data = traps(Mongolia1)[[2]], shape = 3, col = 'firebrick')+
+  coord_equal()+
+  theme_void()
---
+++ Scripts/Simulations.R
@@ -0,0 +1,199 @@
+library(secr)
+library(tidyverse)
+
+source('Scripts/2Encounter_functions.R')
+
+##### Site agnostic simulations #####
+
+simulate_scr_files <- function(spacing = 50, 
+                               lambda0 = 1, 
+                               sigma = 300, 
+                               D = .1, 
+                               d.cov = .1, 
+                               nx_traps = 5, 
+                               trap_spacing = 400, 
+                               detectfn = 'hhn', 
+                               spatial_smooth = 10){
+  
+  raw_trap = expand.grid(x = seq( - ((nx_traps-1)*trap_spacing/2), ((nx_traps-1)*trap_spacing/2),length.out = nx_traps),
+                         y = seq(- ((nx_traps-1)*trap_spacing/2),((nx_traps-1)*trap_spacing/2),length.out = nx_traps))
+  
+  traps = read.traps(data = raw_trap %>% data.frame(.,row.names = paste(1:nrow(raw_trap),'_trap',sep = '')), detector = "count")
+  
+  mesh = make.mask(traps, buffer = 2*sigma, spacing = spacing)
+  
+  if(is.null(d.cov)){
+    
+    sim_pops = sim.popn(D,mesh, buffer = 0)
+    
+  }else{
+    
+    distmat = as.matrix(dist(mesh))
+    
+    V = exp(-distmat/(spacing*spatial_smooth))
+    
+    run = runif(nrow(mesh),min = -1.5,max = 1.5)
+    
+    cov = t(chol(V))%*%run
+    
+    rm(distmat,V)
+    
+    covariates(mesh)$cov = cov
+    set.seed(NULL)
+    sim_pops = sim.popn(D = exp(log(D) + d.cov*cov), model2D = 'IHP', core = mesh)
+  }
+  
+  capthist = sim.capthist(traps,sim_pops,detectfn = 'hhn',detectpar = list(sigma = sigma,lambda0 = lambda0),noccasions = 1,renumber = F)
+  
+  return(list(mesh = mesh, pop = sim_pops,capthist = capthist))
+}
+
+
+
+true_sigma = 300
+true_d1 = .1
+nsims = 100
+
+#####low population, low detections
+set.seed(123)
+
+lowD = .03
+highD = .1
+low_lambda0 = 1
+high_lambda0 = 2
+
+scr_objects = replicate(nsims,simulate_scr_files(D = lowD,
+                                                 d.cov = true_d1, 
+                                                 lambda0 = low_lambda0,
+                                                 sigma = true_sigma,
+                                                 spacing = 50,
+                                                 nx_traps = 5),simplify = F)
+
+mesh = lapply(scr_objects, function(x) x$mesh)
+
+saveRDS(mesh,'Simulations/simulations_mesh.rds')
+
+traps = traps(scr_objects[[1]]$capthist)
+
+ggplot(mesh[[1]], aes(x = x, y = y))+
+  geom_tile(aes(fill = covariates(mesh[[1]])$cov))+
+  geom_point(data = traps)
+
+
+
+
+########### low pop
+lp_pop = lapply(mesh,function(x) sim.popn(D = exp(log(lowD) + true_d1*covariates(x)$cov), model2D = 'IHP', core = x))
+
+########### low detection data
+
+lp_ld_capthists = lapply(lp_pop, function(x) sim.capthist(traps,x,detectfn = 'HHN',detectpar = list(lambda0 = low_lambda0,sigma = true_sigma),noccasions = 1))
+
+lp_ld_dat = c(mean_pop = mean(sapply(lp_pop, nrow)),
+              mean_inds = mean(sapply(lp_ld_capthists,nrow)),
+              mean_dets = mean(sapply(lp_ld_capthists,sum)),
+              mean_recaps = mean(sapply(lp_ld_capthists,max)),
+              mean_sing_dets = mean(sapply(lp_ld_capthists, function(x) sum(rowSums(x)== 1))))
+
+
+###########  high detection data
+
+
+lp_hd_capthists = lapply(lp_pop, function(x) sim.capthist(traps,x,detectfn = 'HHN',detectpar = list(lambda0 = high_lambda0,sigma = true_sigma),noccasions = 1))
+
+lp_hd_dat = c(mean_pop = mean(sapply(lp_pop, nrow)),
+              mean_inds = mean(sapply(lp_hd_capthists,nrow)),
+              mean_dets = mean(sapply(lp_hd_capthists,sum)),
+              mean_recaps = mean(sapply(lp_hd_capthists,max)),
+              mean_sing_dets = mean(sapply(lp_hd_capthists, function(x) sum(rowSums(x)== 1))))
+
+
+
+########### high pop
+hp_pop = lapply(mesh,function(x) sim.popn(D = exp(log(highD) + true_d1*covariates(x)$cov), model2D = 'IHP', core = x))
+
+########### low detection data
+
+hp_ld_capthists = lapply(hp_pop, function(x) sim.capthist(traps,x,detectfn = 'HHN',detectpar = list(lambda0 = low_lambda0,sigma = true_sigma),noccasions = 1))
+
+hp_ld_dat = c(mean_pop = mean(sapply(hp_pop, nrow)),
+              mean_inds = mean(sapply(hp_ld_capthists,nrow)),
+              mean_dets = mean(sapply(hp_ld_capthists,sum)),
+              mean_recaps = mean(sapply(hp_ld_capthists,max)),
+              mean_sing_dets = mean(sapply(hp_ld_capthists, function(x) sum(rowSums(x)== 1))))
+
+
+###########  high detection data
+
+hp_hd_capthists = lapply(hp_pop, function(x) sim.capthist(traps,x,detectfn = 'HHN',detectpar = list(lambda0 = high_lambda0,sigma = true_sigma),noccasions = 1))
+
+hp_hd_dat = c(mean_pop = mean(sapply(hp_pop, nrow)),
+              mean_inds = mean(sapply(hp_hd_capthists,nrow)),
+              mean_dets = mean(sapply(hp_hd_capthists,sum)),
+              mean_recaps = mean(sapply(hp_hd_capthists,max)),
+              mean_sing_dets = mean(sapply(hp_hd_capthists, function(x) sum(rowSums(x)== 1))))
+
+
+
+lp_ld_capthists_ghost = c(lp_ld_capthists,
+                          lapply(lp_ld_capthists, function(x) introduce_ghost(x,.1)),
+                          lapply(lp_ld_capthists, function(x) introduce_ghost(x,.2)),
+                          lapply(lp_ld_capthists, function(x) introduce_ghost(x,.3)))
+
+lp_hd_capthists_ghost = c(lp_hd_capthists,
+                          lapply(lp_hd_capthists, function(x) introduce_ghost(x,.1)),
+                          lapply(lp_hd_capthists, function(x) introduce_ghost(x,.2)),
+                          lapply(lp_hd_capthists, function(x) introduce_ghost(x,.3)))
+
+
+hp_ld_capthists_ghost = c(hp_ld_capthists,
+                          lapply(hp_ld_capthists, function(x) introduce_ghost(x,.1)),
+                          lapply(hp_ld_capthists, function(x) introduce_ghost(x,.2)),
+                          lapply(hp_ld_capthists, function(x) introduce_ghost(x,.3)))
+
+hp_hd_capthists_ghost = c(hp_hd_capthists,
+                          lapply(hp_hd_capthists, function(x) introduce_ghost(x,.1)),
+                          lapply(hp_hd_capthists, function(x) introduce_ghost(x,.2)),
+                          lapply(hp_hd_capthists, function(x) introduce_ghost(x,.3)))
+
+
+
+saveRDS(lp_ld_capthists_ghost,'Simulations/sim_capthists_lp_ld.rds')
+saveRDS(lp_hd_capthists_ghost,'Simulations/sim_capthists_lp_hd.rds')
+saveRDS(hp_ld_capthists_ghost,'Simulations/sim_capthists_hp_ld.rds')
+saveRDS(hp_hd_capthists_ghost,'Simulations/sim_capthists_hp_hd.rds')
+
+
+all_mesh = c(mesh,mesh,mesh,mesh)
+
+lp_ld_scr_fits <- lapply(1:length(lp_ld_capthists_ghost), function(x) tryCatch(secr.fit(lp_ld_capthists_ghost[[x]],all_mesh[[x]],model = list(D~cov), detectfn = 'HHN',start = c(D = log(lowD),D.value = true_d1,lambda0 = log(low_lambda0), sigma = log(true_sigma))),error = function(e) NULL))
+
+saveRDS(lp_ld_scr_fits,'Simulations/simulations_scr_lp_ld.rds')
+
+lp_ld_min2_scr_fits <- lapply(1:length(lp_ld_capthists_ghost), function(x) tryCatch(scr2_lik(lp_ld_capthists_ghost[[x]],all_mesh[[x]],model = list(D~cov), detectfn = 'HHN',startparams = c(D = log(lowD),D.value = true_d1,lambda0 = log(low_lambda0), sigma = log(true_sigma)),simulations = T),error = function(e) NULL))
+
+saveRDS(lp_ld_min2_scr_fits,'Simulations/simulations_scr2_lp_ld.rds')
+
+lp_hd_scr_fits <- lapply(1:length(lp_hd_capthists_ghost), function(x) tryCatch(secr.fit(lp_hd_capthists_ghost[[x]],all_mesh[[x]],model = list(D~cov), detectfn = 'HHN',start = c(D = log(lowD),D.value = true_d1,lambda0 = log(high_lambda0), sigma = log(true_sigma))),error = function(e) NULL))
+
+saveRDS(lp_hd_scr_fits,'Simulations/simulations_scr_lp_hd.rds')
+
+lp_hd_min2_scr_fits <- lapply(1:length(lp_hd_capthists_ghost), function(x) tryCatch(scr2_lik(lp_hd_capthists_ghost[[x]],all_mesh[[x]],model = list(D~cov), detectfn = 'HHN',startparams = c(D = log(lowD),D.value = true_d1,lambda0 = log(high_lambda0), sigma = log(true_sigma)),simulations = T),error = function(e) NULL))
+
+saveRDS(lp_hd_min2_scr_fits,'Simulations/simulations_scr2_lp_hd.rds')
+
+hp_ld_scr_fits <- lapply(1:length(hp_ld_capthists_ghost), function(x) tryCatch(secr.fit(hp_ld_capthists_ghost[[x]],all_mesh[[x]],model = list(D~cov), detectfn = 'HHN',start = c(D = log(highD),D.value = true_d1,lambda0 = log(low_lambda0), sigma = log(true_sigma))),error = function(e) NULL))
+
+saveRDS(hp_ld_scr_fits,'Simulations/simulations_scr_hp_ld.rds')
+
+hp_ld_min2_scr_fits <- lapply(1:length(hp_ld_capthists_ghost), function(x) tryCatch(scr2_lik(hp_ld_capthists_ghost[[x]],all_mesh[[x]],model = list(D~cov), detectfn = 'HHN',startparams = c(D = log(highD),D.value = true_d1,lambda0 = log(low_lambda0), sigma = log(true_sigma)),simulations = T),error = function(e) NULL))
+
+saveRDS(hp_ld_min2_scr_fits,'Simulations/simulations_scr2_hp_ld.rds')
+
+hp_hd_scr_fits <- lapply(1:length(hp_hd_capthists_ghost), function(x) tryCatch(secr.fit(hp_hd_capthists_ghost[[x]],all_mesh[[x]],model = list(D~cov), detectfn = 'HHN',start = c(D = log(highD),D.value = true_d1,lambda0 = log(high_lambda0), sigma = log(true_sigma))),error = function(e) NULL))
+
+saveRDS(hp_hd_scr_fits,'Simulations/simulations_scr_hp_hd.rds')
+
+hp_hd_min2_scr_fits <- lapply(1:length(hp_hd_capthists_ghost), function(x) tryCatch(scr2_lik(hp_hd_capthists_ghost[[x]],all_mesh[[x]],model = list(D~cov), detectfn = 'HHN',startparams = c(D = log(highD),D.value = true_d1,lambda0 = log(high_lambda0), sigma = log(true_sigma)),simulations = T),error = function(e) NULL))
+
+saveRDS(hp_hd_min2_scr_fits,'Simulations/simulations_scr2_hp_hd.rds')
